845dbc92768c4eb2fd132c355ccaae99888ce374
Ellan98 <Ellan@gmail.com>
2024-08-26 18:47:55 +0800

    优化聊天

--- pagesPackage/taskPage/chatRoom/chatRoom.vue
+++ pagesPackage/taskPage/chatRoom/chatRoom.vue
@@ -2,13 +2,13 @@
 
 	<view class="list-container" :style="`height:calc(100vh - ${height}px)`">
 
-		<scroll-view :scroll-top="scrollTop" scroll-y="true" class="container" @scrolltoupper="upper"
-			@scrolltolower="lower" @scroll="scroll" :scroll-with-animation="true"
-			:scroll-into-view=" 'message-' + scrollIntoView">
+		<scroll-view :scroll-top="scrollTop" :style="`height:calc(100vh - ${height}px)`" scroll-y="true"
+			class="container" @scrolltoupper="upper" @scrolltolower="lower" @scroll="scroll"
+			:scroll-with-animation="true" :scroll-into-view=" 'content' + scrollIntoView">
 			<!-- <uni-list :border="true"> -->
 			<template v-for="msg in list">
 				<!-- 右侧带角标 -->
-				<uni-section :title="msg.date" type="line">
+				<uni-section :title="msg.date" type="line" :id="'content' + msg.id">
 					<uni-transition mode-class="fade" :show="true">
 						<uni-list-item class="list-item-container">
 							<template v-slot:header v-if='msg.type == "other"'>
@@ -18,8 +18,10 @@
 							</template>
 							<template v-slot:body>
 								<view class="list-item-container-body">
-									<text
-										:style="msg.type == 'myself'? {float:'right'} : {float:'left'} ">{{msg.content}}</text>
+									<view class="content-Container"
+										:style="msg.type == 'myself'? {float:'right'} : {float:'left'} ">
+										{{msg.content}}
+									</view>
 								</view>
 							</template>
 							<template v-slot:footer v-if='msg.type == "myself"'>
@@ -97,6 +99,29 @@
 		})
 	})
 
+
+const gradually = () => {
+  let index = 0;
+      if (list.value.length === 0) return;
+
+      let currentMessage = '';
+      
+      const intervalId = setInterval(() => {
+        if (index < list.value[list.value.length - 1].fullText.length) {
+          currentMessage += list.value[list.value.length - 1].fullText[index];
+          list.value[list.value.length - 1].content = currentMessage;
+          index++;
+        } else {
+          clearInterval(intervalId);
+         
+        }
+      }, 50); // 每50毫秒更新一次
+}
+
+  
+
+
+
 	// spark model module
 
 	let questions = ref("")
@@ -107,58 +132,69 @@
 	let disabled = ref(false)
 	// 消息 列表
 	let list = ref([])
-	const questionsSparkModel = () => {
+	let index = ref(1)
+	//h5端
+	/*#ifdef H5*/
+	const BASEURL = "/api-dev";
+	/*#endif*/
+	
+	//微信小程序端
+	/*#ifdef MP*/
+	const BASEURL = "https://ellan.online/api/v2";
+	/*#endif*/
+	
+	const questionsSparkModel = async () => {
 
-		disabled.value = true
-		list.value.push({
-			date: date(),
-			type: 'myself',
-			content: questions.value
-		})
-
-
-
-
-		// 关闭之前的连接
-
-
-		const eventSource = new EventSource(`/api-dev/spark/conversation?content=${questions.value.trim()}`);
-		eventSource.onmessage = (e) => {
+  disabled.value = true
 
 
-
-			const newMessage = e.data.content;
-			console.log( JSON.parse(e.data))
-			// showTypingEffect(newMessage);
-
-
-			if (e.data == '[DONE]') {
-				console.log("响应结束")
-				disabled.value = false
-				eventSource.close()
-			}
-		};
-
-		const showTypingEffect = (text) => {
-			let index = 0;
-			const interval = setInterval(() => {
-				if (index < text.length) {
-						let messages = ''
-					messages += text[ index++ ];
-					list.value.push({
-						date: date(),
-						type: 'other',
-						content: messages
-					})
-
-				} else {
-					clearInterval(interval);
-				}
-			}, 50); // 50ms 控制文字显示速度
-		};
+  list.value.push({
+    id: index.value,
+    date: date(),
+    type: 'myself',
+    content: questions.value,
+  })
 
 
 
+  // 关闭之前的连接
+  const eventSource = new EventSource(`${BASEURL}/spark/conversation?content=${questions.value.trim()}`);
+  // 消息 容器
+  let fullText = ""
+  eventSource.onmessage = (e) => {
+
+    fullText += JSON.parse(e.data)["choices"][0]["delta"]["content"] 
+ 
+    console.log("data", JSON.parse(e.data))
+
+ 
+
+
+    if (JSON.parse(e.data)["usage"] ) {
+      console.log("响应结束")
+	  list.value.push({
+	    id: index.value,
+	    date: date(),
+	    type: 'other',
+	    content: '',
+	    fullText: fullText
+	  })
+	  
+	    gradually()
+      disabled.value = false
+	  questions.value=""
+      eventSource.close()
+    }
+
+  }
+
+
+  
+  await nextTick()
+
+
+  scrollIntoView.value = index.value
+  index.value++
 	}
 
 
@@ -230,20 +266,15 @@
 	/* ================== end input style ==================*/
 
 	.chatContainer .uni-list-item__container {
-		border: 1px solid red;
 		height: 100%;
 		padding: 0;
 	}
 
 
 
-	.list-item-container {
-		border: 1px solid black;
-	}
+	.list-item-container {}
 
-	.list-item-container-header {
-		border: 1px solid green;
-	}
+	.list-item-container-header {}
 
 	.list-item-container-header image {
 		border-radius: 10rpx;
@@ -254,15 +285,25 @@
 
 
 	.list-item-container-body {
-		border: 1px solid blue;
 		height: 100%;
 		margin: 30rpx 10rpx;
 		width: calc(100% - 100rpx);
-		/* padding: 10rpx; */
+		padding: 10rpx;
 		text-wrap: wrap;
 	}
 
 	.list-item-container-body .content {
 		float: right;
+	}
+
+	.content-Container {
+		height: 100%;
+		text-wrap: wrap;
+		word-wrap: break-word;
+		/* 允许单词在容器边界处换行 */
+		word-break: break-all;
+		/* 强制在任何字符之间进行断行 */
+		overflow-wrap: break-word;
+		/* 类似于 word-wrap，旧名称 */
 	}
 </style>
--- unpackage/dist/dev/mp-weixin/pagesPackage/taskPage/chatRoom/chatRoom.js
+++ unpackage/dist/dev/mp-weixin/pagesPackage/taskPage/chatRoom/chatRoom.js
@@ -16,6 +16,7 @@ const _easycom_uni_icons = () => "../../../uni_modules/uni-icons/components/uni-
 if (!Math) {
   (_easycom_uni_list_item + _easycom_uni_transition + _easycom_uni_section + _easycom_uni_icons)();
 }
+const BASEURL = "https://ellan.online/api/v2";
 const _sfc_main = {
   __name: "chatRoom",
   setup(__props) {
@@ -31,27 +32,57 @@ const _sfc_main = {
         console.log(res.height);
       });
     });
+    const gradually = () => {
+      let index2 = 0;
+      if (list.value.length === 0)
+        return;
+      let currentMessage = "";
+      const intervalId = setInterval(() => {
+        if (index2 < list.value[list.value.length - 1].fullText.length) {
+          currentMessage += list.value[list.value.length - 1].fullText[index2];
+          list.value[list.value.length - 1].content = currentMessage;
+          index2++;
+        } else {
+          clearInterval(intervalId);
+        }
+      }, 50);
+    };
     let questions = common_vendor.ref("");
     let scrollIntoView = common_vendor.ref("");
     let disabled = common_vendor.ref(false);
     let list = common_vendor.ref([]);
-    const questionsSparkModel = () => {
+    let index = common_vendor.ref(1);
+    const questionsSparkModel = async () => {
       disabled.value = true;
       list.value.push({
+        id: index.value,
         date: utils_date.date(),
         type: "myself",
         content: questions.value
       });
-      const eventSource = new EventSource(`/api-dev/spark/conversation?content=${questions.value.trim()}`);
+      const eventSource = new EventSource(`${BASEURL}/spark/conversation?content=${questions.value.trim()}`);
+      let fullText = "";
       eventSource.onmessage = (e) => {
-        e.data.content;
-        console.log(JSON.parse(e.data));
-        if (e.data == "[DONE]") {
+        fullText += JSON.parse(e.data)["choices"][0]["delta"]["content"];
+        console.log("data", JSON.parse(e.data));
+        if (JSON.parse(e.data)["usage"]) {
           console.log("响应结束");
+          list.value.push({
+            id: index.value,
+            date: utils_date.date(),
+            type: "other",
+            content: "",
+            fullText
+          });
+          gradually();
           disabled.value = false;
+          questions.value = "";
           eventSource.close();
         }
       };
+      await common_vendor.nextTick$1();
+      scrollIntoView.value = index.value;
+      index.value++;
     };
     return (_ctx, _cache) => {
       return {
@@ -73,10 +104,12 @@ const _sfc_main = {
           } : {}, {
             g: "70f386d9-2-" + i0 + "," + ("70f386d9-1-" + i0),
             h: "70f386d9-1-" + i0 + "," + ("70f386d9-0-" + i0),
-            i: "70f386d9-0-" + i0,
-            j: common_vendor.p({
+            i: "content" + msg.id,
+            j: "70f386d9-0-" + i0,
+            k: common_vendor.p({
               title: msg.date,
-              type: "line"
+              type: "line",
+              id: "content" + msg.id
             })
           });
         }),
@@ -85,25 +118,26 @@ const _sfc_main = {
           show: true
         }),
         c: _ctx.scrollTop,
-        d: common_vendor.o((...args) => _ctx.upper && _ctx.upper(...args)),
-        e: common_vendor.o((...args) => _ctx.lower && _ctx.lower(...args)),
-        f: common_vendor.o((...args) => _ctx.scroll && _ctx.scroll(...args)),
-        g: "message-" + common_vendor.unref(scrollIntoView),
-        h: common_vendor.s(`height:calc(100vh - ${common_vendor.unref(height)}px)`),
-        i: common_vendor.p({
+        d: common_vendor.s(`height:calc(100vh - ${common_vendor.unref(height)}px)`),
+        e: common_vendor.o((...args) => _ctx.upper && _ctx.upper(...args)),
+        f: common_vendor.o((...args) => _ctx.lower && _ctx.lower(...args)),
+        g: common_vendor.o((...args) => _ctx.scroll && _ctx.scroll(...args)),
+        h: "content" + common_vendor.unref(scrollIntoView),
+        i: common_vendor.s(`height:calc(100vh - ${common_vendor.unref(height)}px)`),
+        j: common_vendor.p({
           type: "plusempty",
           size: 30
         }),
-        j: common_vendor.p({
+        k: common_vendor.p({
           type: "bars",
           size: 30
         }),
-        k: common_vendor.o((...args) => _ctx.bindTextAreaBlur && _ctx.bindTextAreaBlur(...args)),
-        l: common_vendor.o(linechange),
-        m: common_vendor.unref(questions),
-        n: common_vendor.o(($event) => common_vendor.isRef(questions) ? questions.value = $event.detail.value : questions = $event.detail.value),
-        o: common_vendor.o(questionsSparkModel),
-        p: common_vendor.unref(disabled)
+        l: common_vendor.o((...args) => _ctx.bindTextAreaBlur && _ctx.bindTextAreaBlur(...args)),
+        m: common_vendor.o(linechange),
+        n: common_vendor.unref(questions),
+        o: common_vendor.o(($event) => common_vendor.isRef(questions) ? questions.value = $event.detail.value : questions = $event.detail.value),
+        p: common_vendor.o(questionsSparkModel),
+        q: common_vendor.unref(disabled)
       };
     };
   }
